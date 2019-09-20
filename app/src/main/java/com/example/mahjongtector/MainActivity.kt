6ce4730d4b96803426a8d945de130d933f83c120
s-sakai-cw <s-sakai-cw>
2019-09-20 15:15:57 +0900

    hozonn

--- app/src/main/java/com/example/mahjongtector/MainActivity.kt
+++ app/src/main/java/com/example/mahjongtector/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.mahjongtector
 
+import android.Manifest
+import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
+import android.net.Uri;
+import android.provider.MediaStore
+import android.util.Log
+import android.view.View;
+import android.widget.*
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.io.File
+
+
 class MainActivity : AppCompatActivity() {
+
+
+    var task: UploadTask? = null
+    var textView: TextView? = null
+    var editText: EditText? = null
+
+    var url = "http://127.0.0.1:8887/pass_check.html"
+
+    val RESULT_CAMERA = 1001
+    var imageView:ImageView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,24 +40,39 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-//        // 非同期処理
-//        "https://www.casareal.co.jp/".httpGet().response { request, response, result ->
-//            when (result) {
-//                is Result.Success -> {
-//                    // レスポンスボディを表示
-//                    println("非同期処理の結果：" + String(response.data))
-//                }
-//                is Result.Failure -> {
-//                    println("通信に失敗しました。")
-//                }
-//            }
-//        }
-//
-//        // 同期処理
-//        val triple = "https://www.casareal.co.jp/".httpGet().response()
-//        // レスポンスボディを表示
-//        println("同期処理の結果：" + String(triple.second.data))
+        imageView = findViewById(R.id.image_view)
 
+        val cameraButton: Button = findViewById(R.id.camera_button)
+        cameraButton.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View) {
+                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivityForResult(intent, RESULT_CAMERA)
+            }
+        })
     }
-}
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == RESULT_CAMERA) {
+            val bitmap: Bitmap?
+            // cancelしたケースも含む
+            if (data!!.getExtras() == null) {
+                Log.d("debug", "cancel ?")
+                return
+            } else {
+                bitmap = data!!.getExtras()!!.get("data") as Bitmap
+                if (bitmap != null) {
+                    // 画像サイズを計測
+                    val bmpWidth = bitmap!!.getWidth()
+                    val bmpHeight = bitmap!!.getHeight()
+                    Log.d("debug", String.format("w= %d", bmpWidth))
+                    Log.d("debug", String.format("h= %d", bmpHeight))
+                }
+            }
+
+            imageView!!.setImageBitmap(bitmap)
+        }
+    }
+
+
+
+}
